c14a2658907fda21bd8c9a90605214e48ae8329c
Apurbo-hue <labunna123@gmail.com>
2026-01-07 15:28:53 +0600

    sort functionality added

--- src/Pages/ReadLIst/ReadList.jsx
+++ src/Pages/ReadLIst/ReadList.jsx
@@ -7,8 +7,12 @@ import { getStoredBook } from '../../utilites/addToDB';
 
 const ReadList = () => {
   const datas = useLoaderData()
-  console.log(datas);
-  const [readLists, setReadLists] = useState([])
+
+
+  const [readLists, setReadLists] = useState([]);
+  const [sortBy, setSortBy] = useState("");
+
+
   const loadedDatas = useEffect(
     () => {
       const storedBooksData = getStoredBook();
@@ -22,15 +26,45 @@ const ReadList = () => {
     }, []
   )
 
+  const handleSortBy = (type) =>{
+
+    setSortBy(type);
+
+    if (type === "pages")
+    {
+      const sortByPage = [...readLists].sort((a,b)=>a.totalPages-b.totalPages);
+      setReadLists(sortByPage);
+    }
+    else if(type ==="rating")
+    {
+      const sortByRating = [...readLists].sort((a,b)=>a.rating-b.rating);
+      setReadLists(sortByRating);
+    }
+  }
+
   return (
+
     <Tabs>
+
       <TabList>
         <Tab>Read list</Tab>
 
       </TabList>
 
       <TabPanel>
-        <button onClick={() => localStorage.setItem("readList", [])} className='btn bg-red-600 mr-2'>Clear Readlist</button>
+        <div className='flex justify-between'>
+
+          <details className="dropdown">
+            <summary className="btn mb-2 p-2">Sort By : {sortBy?sortBy:""}</summary>
+            <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+              <li onClick={() => handleSortBy("pages")}><a>Pages no.</a></li>
+              <li onClick={() => handleSortBy("rating")}><a>Rating</a></li>
+            </ul>
+          </details>
+
+          <button onClick={() => localStorage.setItem("readList", [])} className='btn bg-red-600 mr-2'>Clear Readlist</button>
+
+        </div>
 
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
           {readLists.map(book => <Book key={book.bookId} book={book}></Book>)}
